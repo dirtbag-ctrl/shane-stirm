7d243de71c8e36f65500b137ba55c3cf9f2b41d9
Gilbert Young <jdirtbag20@gmail.com>
2021-03-26 12:20:58 -0600

    second try continuous deployment

--- src/App.test.js
+++ src/App.test.js
@@ -6,9 +6,9 @@ import App from "./App";
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
-    <Provider store={store}>
+    <React.StrictMode>
       <App />
-    </Provider>,
+    </React.StrictMode>,
     div
   );
 });
--- src/components/HorizontalGallery.js
+++ src/components/HorizontalGallery.js
@@ -1,35 +1,43 @@
 import React from "react";
-
+import img1 from "../assets/img4.jpg";
 export default function HorizontalGallery() {
   return (
-    <div class="container">
-      <div class="container__item container__item-header">
-        <div class="container__item container__item-1">
-          <h1 class="container__item-title">
-            <a href="project.html">Asterisks</a>
-          </h1>
-        </div>
-        <div class="container__item container__item-2">
-          <h1 class="container__item-title">
-            <a href="project.html">Unlocked</a>
-          </h1>
-        </div>
-        <div class="container__item container__item-3">
-          <h1 class="container__item-title">
-            <a href="project.html">Offbeat</a>
-          </h1>
-        </div>
-        <div class="container__item container__item-4">
-          <h1 class="container__item-title">
-            <a href="project.html">Brutal</a>
-          </h1>
-        </div>
-        <div class="container__item container__item-5">
-          <h1 class="container__item-title">
-            <a href="project.html">Mouthwash</a>
-          </h1>
-        </div>
+    <div className="container">
+      <div className="slider">
+        <img className="img" src={img1} alt="" />
+        <img className="img" src={img1} alt="" />
+        <img className="img" src={img1} alt="" />
+        <img className="img" src={img1} alt="" />
       </div>
     </div>
+    // <div class="container">
+    //   <div class="container__item container__item-header">
+    //     <div class="container__item container__item-1">
+    //       <h1 class="container__item-title">
+    //         <a href="project.html">Asterisks</a>
+    //       </h1>
+    //     </div>
+    //     <div class="container__item container__item-2">
+    //       <h1 class="container__item-title">
+    //         <a href="project.html">Unlocked</a>
+    //       </h1>
+    //     </div>
+    //     <div class="container__item container__item-3">
+    //       <h1 class="container__item-title">
+    //         <a href="project.html">Offbeat</a>
+    //       </h1>
+    //     </div>
+    //     <div class="container__item container__item-4">
+    //       <h1 class="container__item-title">
+    //         <a href="project.html">Brutal</a>
+    //       </h1>
+    //     </div>
+    //     <div class="container__item container__item-5">
+    //       <h1 class="container__item-title">
+    //         <a href="project.html">Mouthwash</a>
+    //       </h1>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
